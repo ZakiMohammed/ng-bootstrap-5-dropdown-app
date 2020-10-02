8d8c8fd82450b2ca14ea975f14374e9e681f80bd
Zaki Shaikh <zaki-shaikh@outlook.com>
2020-10-03 02:08:14 +0530

    Title updated, gitignore dist added, multi drpodown test updated

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,23 +13,4 @@ describe('AppComponent', () => {
       ],
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'dropdown-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('dropdown-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('dropdown-app app is running!');
-  });
 });
--- src/app/shared/multi-dropdown/multi-dropdown.component.spec.ts
+++ src/app/shared/multi-dropdown/multi-dropdown.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MultiDropdownComponent } from './multi-dropdown.component';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { v4 } from 'uuid';
 
 describe('MultiDropdownComponent', () => {
   let component: MultiDropdownComponent;
@@ -8,7 +10,8 @@ describe('MultiDropdownComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MultiDropdownComponent ]
+      declarations: [ MultiDropdownComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
@@ -16,10 +19,95 @@ describe('MultiDropdownComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(MultiDropdownComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    component.items = [
+        {
+          uuid: v4(),
+          id: 4,
+          name: 'Apple',
+          checked: true,
+          visible: true
+        }, {
+          uuid: v4(),
+          id: 5,
+          name: 'Mango',
+          checked: true,
+          visible: true
+        }, {
+          uuid: v4(),
+          id: 6,
+          name: 'Banana',
+          checked: true,
+          visible: true
+        }
+      ];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should filter list on search', () => {
+    component.search = 'Apple';
+    expect(component.filterList.length).toEqual(1);
+    expect(component.filterList[0].name).toEqual(component.search);
+  });
+
+  it('should not filter list on empty search', () => {
+    component.search = '';
+    expect(component.filterList.length).toEqual(component.items.length);
+  });
+
+  it('should get selected item names from list', () => {
+    component.items[0].checked = false;
+    component.items[0].visible = false;
+
+    component.items[component.items.length - 1].checked = false;
+    component.items[component.items.length - 1].visible = false;
+
+    expect(component.selected).toEqual(component.items.map(i => i.name).slice(1, component.items.length - 1).join(', '));
+  });
+
+  it('should return true if nothing is visible in the list', () => {
+    component.items.forEach(i => i.visible = false);
+    expect(component.isEmpty).toBeTrue();
+  });
+
+  it('should return false if something is visible in the list', () => {
+    fixture.detectChanges();
+    expect(component.isEmpty).toBeFalse();
+  });
+
+  it('should initialize component onInit', () => {
+    fixture.detectChanges();
+    expect(component.filterList.length).toEqual(component.items.length);
+  });
+
+  it('should emit itemChange output event onChange', () => {
+    spyOn(component.itemChange, 'emit');
+    component.onChange({
+      target: {
+        checked: true
+      }
+    }, {...component.items[0]});
+    expect(component.itemChange.emit).toHaveBeenCalled();
+  });
+
+  it('should check item incase of unchecked onChange', () => {
+    component.items[1].checked = false;
+    component.onChange({
+      target: {
+        checked: true
+      }
+    }, {...component.items[1]});
+    expect(component.items[1].checked).toBeTrue();
+  });
+
+  it('should un-check item incase of checked onChange', () => {
+    component.onChange({
+      target: {
+        checked: false
+      }
+    }, {...component.items[1]});
+    expect(component.items[1].checked).toBeFalse();
   });
 });
